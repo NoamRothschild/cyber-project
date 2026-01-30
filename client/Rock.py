c10943eb340b1a3248d8eb2e42e622d614cb4592
along <alonguthrie4@gmail.com>
2026-01-30 15:12:46 +0200

    made a map 40 screens on 40 screens according to map.png

--- client/Rock.py
+++ client/Rock.py
@@ -1,8 +1,8 @@
 import pygame
 from mapset import *
 class Rock(pygame.sprite.Sprite):#rock obstacle sprites
-    def __init__(self,pos,groups):
+    def __init__(self,pos,groups,image):
         super().__init__(groups)
-        self.image = pygame.image.load('rock.png').convert_alpha()
+        self.image = image
         self.image.set_colorkey(PINK)
         self.rect = self.image.get_rect(topleft=pos)
--- client/level.py
+++ client/level.py
@@ -4,6 +4,7 @@ from Entity import Entities
 from mapset import *
 from Player import *
 from Rock import Rock
+from PIL import Image
 class level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
@@ -11,27 +12,36 @@ class level:
         self.visible_sprites = Camera()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        self.image = [pygame.image.load('rock.png').convert_alpha(), pygame.image.load('tree.png').convert_alpha(),
+                      pygame.image.load('water.png').convert_alpha()]
+
         self.entities = Entities()
 
         self.draw_map()
 
     def draw_map(self):#crating a very basic map with small borders(need to be changed
-        self.visible_sprites.update()
-        for i in range(WIDTH*40//size-size):
-            Rock((i*size, 0), [self.visible_sprites, self.obstacle_sprites])
-            Rock((i * size, 100*size), [self.visible_sprites, self.obstacle_sprites])
-        for i in range(HEIGHT*40//size-size):
-            Rock((0, i * size), [self.visible_sprites, self.obstacle_sprites])
-            Rock((100*size,i * size ), [self.visible_sprites, self.obstacle_sprites])
-        for rindex,row in enumerate(world_map):
-            for cindex,col in enumerate(row):
-                x=cindex*size+2*size
-                y=rindex*size+2*size
-                if col=='x':
-                    Rock((x,y),[self.visible_sprites,self.obstacle_sprites])
+        self.map_image = Image.open("map.png")
+        pixels = self.map_image.load()
+        width, height = self.map_image.size
+        tree_count = 0
 
-                if col=='p':
-                    self.player=Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+        for x in range(width):
+            for y in range(height):
+                # בדיקה אם יש ערוץ אלפא (RGBA) או רק RGB
+                pixel = pixels[x, y]
+                r, g, b = pixel[:3]
+
+                if r == 0 and g == 162 and b == 232:
+                    Rock((x * size, y * size), [self.visible_sprites, self.obstacle_sprites], self.image[2])
+                elif r == 120 and g == 67 and b == 21:
+                    Rock((x * size, y * size), [self.visible_sprites, self.obstacle_sprites], self.image[0])
+                elif r == 24 and g == 62 and b == 12:
+                    if tree_count % 7 == 0:
+                        Rock((x * size, y * size), [self.visible_sprites, self.obstacle_sprites], self.image[1])
+                    tree_count += 1
+
+        # השחקן נוצר פעם אחת בלבד - מחוץ ללולאה
+        self.player = Player((640 * size, 260 * size), [self.visible_sprites], self.obstacle_sprites)
 
 
     def run(self):
@@ -50,6 +60,9 @@ class Camera(pygame.sprite.Group):# a group that has every visible sprite that s
     def custom_draw(self,player):
         self.point.x=player.rect.centerx-self.half_width
         self.point.y=player.rect.centery-self.half_height
+        screen_rect = pygame.Rect(self.point.x, self.point.y, self.display.get_width(), self.display.get_height())
+
         for sprite in self.sprites():
-            point_pos= sprite.rect.topleft-self.point
-            self.display.blit(sprite.image,point_pos)
+            if sprite.rect.colliderect(screen_rect):
+                point_pos= sprite.rect.topleft-self.point
+                self.display.blit(sprite.image,point_pos)
