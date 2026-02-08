d266991fd1a825c214de745fc8edb770ec8ff44d
Idan <Idankost4321@gmail.com>
2026-02-08 21:54:10 +0200

    added handle login function added checks for login and register added session id implementation

--- TCP.py
+++ TCP.py
@@ -1,20 +1,24 @@
 import socket
 import sqlite3
 import hashlib
+import uuid
 
 DB_NAME = 'Auth.db'
 PORT = 9999
 IP = '127.0.0.1'
 BYTES_TO_DECODE = 1024
 
+
 def get_db_connection():
     """Creates a fresh database connection."""
     return sqlite3.connect(DB_NAME)
+
 
 def get_hashed_password(username, password):
     """Hashes the password with the username as a salt."""
     hashed_object = hashlib.sha256((username + password).encode("utf-8"))
     return hashed_object.hexdigest()
+
 
 def create_table():
     """Initializes the database tables."""
@@ -69,6 +73,32 @@ def handle_register(username, password):
         print(f"Error registering user: {e}")
         return "REGISTER_FAILED"
 
+
+def handle_login(username, password):
+    try :
+        with get_db_connection() as connection:
+            cursor = connection.cursor()
+            hashed_pw = get_hashed_password(username, password)
+            cursor.execute("SELECT user_ID FROM USERS WHERE username = ? and password = ?", (username,hashed_pw))
+
+            result = cursor.fetchone()
+            if result:
+                user_id_from_db = result[0]
+                session_id = str(uuid.uuid4())
+
+                cursor.execute("DELETE FROM SESSIONS WHERE user_id = ?", (user_id_from_db,))
+                cursor.execute("INSERT INTO SESSIONS (session_id, user_id) VALUES (?, ?)",
+                               (session_id, user_id_from_db))
+                connection.commit()
+                return f"LOGIN_SUCCESS:{session_id}"
+            else:
+                print("You are not logged in. you need to register first.")
+                return "LOGIN_FAILED"
+    except sqlite3.Error as e:
+        print(f"Error registering user: {e}")
+        return "LOGIN_FAILED"
+
+
 def run_server():
      server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
      server_socket.bind((IP, PORT))
@@ -81,19 +111,28 @@ def run_server():
          try:
              data = client_socket.recv(BYTES_TO_DECODE).decode('utf-8')
              if ":" in data:
-                 username, password = data.split(":")
+                 command,username, password = data.split(":", 2)
 
-                 result = handle_register(username, password)
+                 if command == "REG":
+                    result = handle_register(username, password)
+                    if result == "REGISTER_SUCCESS":
+                        print(f"User {username} successfully registered/pushed!")
+                        client_socket.send("AUTH_SUCCESS".encode())
+                    elif result == "REGISTER_TAKEN":
+                        print(f"User {username} tried to register but already exists.")
+                        client_socket.send("AUTH_TAKEN".encode())
+                    else:
+                        print("Database error occurred.")
+                        client_socket.send("AUTH_FAILED".encode())
 
-                 if result == "REGISTER_SUCCESS":
-                     print(f"User {username} successfully registered/pushed!")
-                     client_socket.send("AUTH_SUCCESS".encode())
-                 elif result == "REGISTER_TAKEN":
-                     print(f"User {username} tried to register but already exists.")
-                     client_socket.send("AUTH_TAKEN".encode())
-                 else:
-                    print("Database error occurred.")
-                    client_socket.send("AUTH_FAILED".encode())
+                 elif command == "LOG":
+                    result = handle_login(username, password)
+
+                    if result.startswith("LOGIN_SUCCESS"):
+                        print(f"User {username} logged in.")
+                        client_socket.send(result.encode())  # Send back the full string with session ID
+                    elif result == "LOGIN_FAILED":
+                        client_socket.send("LOGIN_FAILED".encode())
          finally:
              client_socket.close()
 
