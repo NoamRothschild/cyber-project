d1a880d83e5ce9a5e2c8e06491cc3ea6d6193ff7
gil <eingar.gil@gmail.com>
2026-02-11 10:16:45 +0200

    cntrl alt l

--- client/entity.py
+++ client/entity.py
@@ -3,8 +3,11 @@ from typing import Any, Dict, Tuple
 from mapset import PINK
 import pygame
 from helth import HealthBar
-# TODO: display entitys hp bar above them
-SCALE_FROM_LIFE=5
+
+# TODO: display entity's hp bar above them
+SCALE_FROM_LIFE = 5
+
+
 class Entity(pygame.sprite.Sprite):
     def __init__(self, groups: Any, pos: Tuple[int, int]) -> None:
         super().__init__(groups)
@@ -14,8 +17,8 @@ class Entity(pygame.sprite.Sprite):
         # pos is the hitbox position (matching what Player sends)
         self.hitbox = pygame.Rect(pos[0], pos[1], self.rect.width - 20, self.rect.height - 10)
         self.rect.center = self.hitbox.center
-        self.hp = 400 # TODO: fetch from config
-        self.hp_b = HealthBar((self.hitbox.x,self.hitbox.y-10),self.hp//SCALE_FROM_LIFE,groups[0])
+        self.hp = 400  # TODO: fetch from config
+        self.hp_b = HealthBar((self.hitbox.x, self.hitbox.y - 10), self.hp // SCALE_FROM_LIFE, groups[0])
 
     def move(self, new_pos: None | Tuple[int, int]):
         if new_pos is None:
@@ -23,16 +26,17 @@ class Entity(pygame.sprite.Sprite):
         self.hitbox.x = new_pos[0]
         self.hitbox.y = new_pos[1]
         self.rect.center = self.hitbox.center
-        self.hp_b.move([new_pos[0],new_pos[1]-10])
-    
+        self.hp_b.move([new_pos[0], new_pos[1] - 10])
+
     def set_hp(self, hp: int | None = None):
         if hp is None:
             return
-        if hp>self.hp:
-            self.hp_b.add_life(hp//SCALE_FROM_LIFE-self.hp_b.get_life())
-        elif hp<self.hp:
-            self.hp_b.sub_life(self.hp_b.get_life()-hp//SCALE_FROM_LIFE)
+        if hp > self.hp:
+            self.hp_b.add_life(hp // SCALE_FROM_LIFE - self.hp_b.get_life())
+        elif hp < self.hp:
+            self.hp_b.sub_life(self.hp_b.get_life() - hp // SCALE_FROM_LIFE)
         self.hp = hp
+
 
 class Entities:
     def __init__(self) -> None:
